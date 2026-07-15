652bd9a388e65212e873c4f2034e0e938fe5966b
darshanjain18 <jaindarshan1804@gmail.com>
2026-07-15 20:57:55 +0530

    Initial commit

--- Desktop/automation testing/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture
-def driver():
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-    yield browser
-    browser.quit()
